7a186fdb63e861082ebbb46b0f4706cc1bada35f
Shubham Shanker <shub6221@gmail.com>
2023-11-18 13:27:12 +0530

    file add

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+  2
+  3 import "fmt"
+  4
+  5 func main(){
+  6
+  7   i := 0
+  8   for i <= 10{
+  9     fmt.Println("i = ",i)
+ 10     i += 1
+ 11   }
+ 12   fmt.Println("Hello")
+ 13 }
+ 14
